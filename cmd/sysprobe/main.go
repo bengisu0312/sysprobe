7ace84b401d8ed072708176a8cb4c61d5e828ded
Bengisu Uçar <ucarbengisu03@gmail.com>
2026-09-15 16:28:42 +0300

    feat(cli): exit with nagios-compatible status codes

--- cmd/sysprobe/main.go
+++ cmd/sysprobe/main.go
@@ -8,9 +8,11 @@ import (
 
 	"github.com/bengisu0312/sysprobe/internal/collector"
 	"github.com/bengisu0312/sysprobe/internal/config"
+	"github.com/bengisu0312/sysprobe/internal/rules"
 )
 
 func main() {
+	// os.Exit sadece burada çağrılır. Bu sayede içerideki defer'lar ezilmez.
 	os.Exit(run(os.Args, os.Stdout, os.Stderr))
 }
 
@@ -24,25 +26,23 @@ func run(args []string, stdout, stderr io.Writer) int {
 
 	switch subcommand {
 	case "status":
-		// Flag okumak için Go'nun standart 'flag' kütüphanesini kullanıyoruz
 		statusCmd := flag.NewFlagSet("status", flag.ContinueOnError)
 		statusCmd.SetOutput(stderr)
 		configFlag := statusCmd.String("config", "", "Config dosyasi yolu")
 
-		// args[2:] çünkü args[0]=sysprobe, args[1]=status
 		if err := statusCmd.Parse(args[2:]); err != nil {
 			return 2
 		}
 
-		// Config'i yüklüyoruz
 		cfg, err := config.Load(*configFlag)
 		if err != nil {
 			fmt.Fprintf(stderr, "Config yukleme hatasi: %v\n", err)
-			return 1
+			return 1 // Hata durumu
 		}
 
-		// Config'in yüklendiğini kanıtlamak için ekrana basalım
 		fmt.Fprintf(stdout, "[Config Loaded] CPU Warn: %.1f | Disk Warn: %.1f\n\n", *cfg.Thresholds.CPUWarning, *cfg.Thresholds.DiskWarning)
+
+		var allMetrics []collector.Metric
 
 		for _, c := range collector.DefaultCollectors() {
 			metrics, err := c.Collect()
@@ -50,11 +50,21 @@ func run(args []string, stdout, stderr io.Writer) int {
 				fmt.Fprintf(stderr, "collector %s failed: %v\n", c.Name(), err)
 				continue
 			}
+			
+			// Toplananları listeye ekle
+			allMetrics = append(allMetrics, metrics...)
+			
 			for _, m := range metrics {
 				fmt.Fprintf(stdout, "%-20s %8.1f %s\n", m.Name, m.Value, m.Unit)
 			}
 		}
-		return 0
+
+		// Kural motorunu çalıştır ve son durumu bul
+		overallStatus := rules.Evaluate(allMetrics, cfg)
+		fmt.Fprintf(stdout, "\nOVERALL STATUS: %s\n", overallStatus)
+
+		// Nagios standardına uygun Exit Code döndür (OK=0, WARN=1, CRIT=2)
+		return int(overallStatus)
 
 	case "version":
 		fmt.Fprintln(stdout, "sysprobe dev")
